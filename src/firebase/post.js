d02dcd5d738c5138ac99ad3f27a5f922377702a4
Miriam Cortes <miriamcortes.06@gmail.com>
2022-10-11 09:54:50 -0500

    onSnapshot

--- src/firebase/post.js
+++ src/firebase/post.js
@@ -1,11 +1,17 @@
-import { collection, addDoc, getFirestore } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
+import {
+  collection, addDoc, getFirestore, onSnapshot,
+} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
 import { app } from './config.js';
 
 const db = getFirestore(app);
 
-export const savePost = async () => {
+export const savePost = async (textPost) => {
   const docRef = await addDoc(collection(db, 'post'), {
-    message: 'Hi Mortaluchos',
+    message: textPost,
   });
   console.log('Document written with ID: ', docRef.id);
+};
+
+export const getPosts = (callback) => {
+  onSnapshot(collection(db, 'post'), callback);
 };
