019b9088cd5158f3946c420b1aa1546b9c1dff6a
Miriam Cortes <miriamcortes.06@gmail.com>
2022-10-12 00:01:24 -0500

    vista muro

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -5,17 +5,25 @@ import { createPost } from './createPost.js';
 import { getPosts } from '../firebase/post.js';
 
 export const Wall = () => {
-  const div = document.createElement('div');
-  const title = document.createElement('h2');
-  title.textContent = 'MangaReads';
-  title.classList = ('MangaReads');
-  const buttonSignOut = document.createElement('button');
-  buttonSignOut.textContent = 'Sign Out';
-  buttonSignOut.classList.add('Back');
-  const containerPost = document.createElement('div');
-
+  const div = document.createElement('div'); // Contenedor principal
   div.classList.add('container-wall');
 
+  const titleW = document.createElement('h2');
+  titleW.textContent = 'MangaReads';
+  titleW.classList.add('titleW');
+
+  const title2W = document.createElement('h3');
+  title2W.textContent = 'まんが';
+  title2W.classList.add('title2W');
+
+  const textIntroW = document.createElement('h4');
+  textIntroW.textContent = 'Write your post';
+  textIntroW.classList.add('textIntroW');
+
+  const buttonSignOut = document.createElement('button');
+  buttonSignOut.textContent = 'Sign Out';
+  buttonSignOut.classList.add('buttonS');
+  const containerPost = document.createElement('div');
   buttonSignOut.addEventListener('click', () => {
     //  onNavigate('/'); // Sign-out successful.
     signOutAccount(auth).then(() => {
@@ -32,13 +40,14 @@ export const Wall = () => {
       const post = doc.data();
       const postContent = document.createElement('p');
       postContent.textContent = post.message;
-      postContent.classList.add = 'WallView';
+      postContent.classList.add ('WallView');
+      containerPost.classList.add ('container');
       console.log(post);
       containerPost.append(postContent);
     });
   });
 
-  div.append(title, buttonSignOut, containerPost, createPost());
+  div.append(titleW, title2W, textIntroW, buttonSignOut, containerPost, createPost());
 
   return div;
 };
